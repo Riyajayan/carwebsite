fdefee995ef7b00bc7472d4d75a64a9fc55b3b19
Riyajayan <149082184+Riyajayan@users.noreply.github.com>
2023-10-28 12:01:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    menu.classList.remove('active');
+}
+let navbar = document.querySelector('.navbar');
+let menu = document.querySelector('#menu-icon');
+
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    menu.classList.toggle('fa-times')
+    search.classList.remove('active');
+}
+
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () =>{
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+    
+    
